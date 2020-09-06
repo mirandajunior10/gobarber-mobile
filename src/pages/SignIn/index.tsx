13d1d68661cc3c9f99b0ba4194871c607c320dca
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-06 14:39:36 -0300

    Iniciando aplicativo mobile : Módulo 2 - Input & Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Image } from 'react-native';
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   const um = 1;
@@ -9,6 +11,9 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button>Entrar</Button>
     </Container>
   );
 };
