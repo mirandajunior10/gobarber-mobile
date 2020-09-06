9de0d2a8edd388d162b0e6c4f67af2ea039c9aa8
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-06 11:45:23 -0300

    Iniciando aplicativo mobile : Módulo 1 -Importando fontes externas

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,7 +1,14 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
 
-export const Text = styled.Text`
-  color: #fff;
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
 `;
