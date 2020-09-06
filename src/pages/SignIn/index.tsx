afd529cb9d107a1f7c237de303a58fc7bf294542
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-06 11:26:50 -0300

    Iniciando aplicativo mobile : Módulo 1 - Densidade de pixel

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
-const SignIn: React.FC = () => <Container />;
+const SignIn: React.FC = () => {
+  const um = 1;
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
+};
 
 export default SignIn;
