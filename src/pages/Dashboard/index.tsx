f74fe6b52e611ff950287a8ae4422b83d845f6b4
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2021-03-14 14:58:46 -0300

    Finalizando front-end do mobile : Módulo 2 -Buscando providers da API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 import {
   Container,
   Header,
@@ -8,15 +9,31 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
 
   const navigateToProfile = useCallback(() => {
-    navigate('Profile');
-  }, [navigate]);
+    signOut();
+    // navigate('Profile');
+  }, [signOut]);
+
+  useEffect(() => {
+    api.get<Provider[]>('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
 
   return (
     <Container>
@@ -30,6 +47,11 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
     </Container>
   );
 };
